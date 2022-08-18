8aa149d13c409bf32047a3377d7b457f0b2dc34b
Pavel <wol1414@gmail.com>
2022-08-18 03:04:31 +0300

    start filter

--- public/js/common.js
+++ public/js/common.js
@@ -595,6 +595,38 @@ function eventHandler() {
 
 		;
 	});
+
+
+
+
+
+	const convertImages = (query, callback) => {
+		const images = document.querySelectorAll(query);
+	
+		images.forEach(image => {
+			fetch(image.src)
+			.then(res => res.text())
+			.then(data => {
+				const parser = new DOMParser();
+				const svg = parser.parseFromString(data, 'image/svg+xml').querySelector('svg');
+	
+				if (image.id) svg.id = image.id;
+				if (image.className) svg.classList = image.classList;
+	
+				image.parentNode.replaceChild(svg, image);
+			})
+			.then(callback)
+			.catch(error => console.error(error))
+		});
+	}
+
+	convertImages('.img-to-svg');
+	$(".sFilter__toggle").click(function(){
+		$(".sFilter__toggle").toggleClass("active");
+		$('.sFilter__dropdown').toggleClass("active");
+
+	})
+
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
